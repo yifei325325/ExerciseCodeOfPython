0ab5d7bf0cbd2bff543ca49f2c842ea6eaf84e5d
yifei325325 <yifei1193@163.com>
2015-12-11 00:09:51 +0800

    小修改

--- MyTest/com/testRequests.py
+++ MyTest/com/testRequests.py
@@ -1,11 +1,28 @@
 #!/usr/bin/python
 #coding:utf8
 '''
-Created on 2015��12��10��
+Created on 2015年12月10日
 
 @author: liyutong
 '''
-import requests
+import requests,time
 
-r = requests.get("www.baidu.com")
-print r.status_code
+url = 'http://www.ibabyclouds.com'
+try:
+    r = requests.get(url)
+    if r.status_code == 200:
+        print "服务器返回'%d',网络连接正常！" %r.status_code
+        try:
+            f = open('log.txt','w')
+            f.writelines(r.text.encode('utf8'))
+            f.close()
+            print '返回的html源码请看log.txt'
+        except Exception,e:
+            print e
+    else:
+        print 'r.status_code:',r.status_code
+except Exception,e:
+    print time.strftime('%H:%M:%S',time.localtime()),'\t',e
+    print '%s\twrong address'%time.strftime('%H:%M:%S',time.localtime())
+
+  
