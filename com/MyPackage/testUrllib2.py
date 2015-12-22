e5483073ef0e786c56da7217baecf59b96951535
Kenny.Li <Kenny.Li@KennyLi-PC>
2015-12-22 14:06:14 +0800

    防爬网站增加请求头

--- com/MyPackage/testUrllib2.py
+++ com/MyPackage/testUrllib2.py
@@ -9,13 +9,15 @@ Created on 2015年12月18日
 import urllib2,urllib
 
 url = 'http://blog.csdn.net/liyuanbhu/article/details/50365287'
-req = urllib2.Request(url)
 
+my_headers = {'User-Agent':'Mozilla/5.0 (Windows NT 6.3; WOW64)\
+                AppleWebKit/537.36 (KHTML, like Gecko) Maxthon/4.4.8.1000 Chrome/30.0.1599.101 Safari/537.36',
+                "Referer":"http://blog.csdn.net/",
+                "Host":"blog.csdn.net",
+                "GET":url
+              }
 #添加防爬网站必须的请求头
-req.add_header('User-Agent','Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Maxthon/4.4.8.1000 Chrome/30.0.1599.101 Safari/537.36')
-req.add_header("Referer","http://blog.csdn.net/")
-req.add_header("Host","blog.csdn.net")
-req.add_header("GET",url)
+req = urllib2.Request(url,headers=my_headers)
 
 html = urllib2.urlopen(req)
 print html.read()
