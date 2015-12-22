e276bb3cef81ebb903b825a760b3b45eb78d5f9d
kenny.li <kenny.li@kenny>
2015-12-22 21:24:35 +0800

    没改啥

--- com/MyPackage/jiaMiAndJieMi.py
+++ com/MyPackage/jiaMiAndJieMi.py
@@ -6,9 +6,9 @@ Created on 2015年12月22日
 @author: Kenny.Li
 '''
 import RNCryptor,base64
-s = raw_input("input the encrypted string:")
-# s = "AwFsMT4MseEK7S08non9ND3MghWLrkTzOxtpp+56xP4EpDOqeG4M1n1wsXOo/MNxzgn+fiDH81EmlN9rkdvFvX1r8Swfb33PKHfmhy95Fo83UP4CdgudRVl0/WChlA8QxquBtIlVSg4KfxlyyDRAHqIl9A7Z+qOrPZkBtnsphiFvbvwMlyRqpgMX3f85JRDLg9ICEaeqLZnaeMEtp3zsOUMRLZwRDPPVf496ujBwPYNWvD66lKtPbcz+2C/J9EF/yGxCcoih0FcLF5vzCrDhVbTlwb1zvlDXmFj63st8hmm6DtuoS+b6IBFjJuLJ+gbrIxncpqMG6wecQYopXn3dYQSjWezyvffESHbg55XlfmF1PG4RNAvk9tHB94H1lISX4bF1TLMe+dItIbCzrBPKFqBV"
-# s = "test"
+
+# s = raw_input("input the encrypted string:\n")
+s = "AwFsMT4MseEK7S08non9ND3MghWLrkTzOxtpp+56xP4EpDOqeG4M1n1wsXOo/MNxzgn+fiDH81EmlN9rkdvFvX1r8Swfb33PKHfmhy95Fo83UP4CdgudRVl0/WChlA8QxquBtIlVSg4KfxlyyDRAHqIl9A7Z+qOrPZkBtnsphiFvbvwMlyRqpgMX3f85JRDLg9ICEaeqLZnaeMEtp3zsOUMRLZwRDPPVf496ujBwPYNWvD66lKtPbcz+2C/J9EF/yGxCcoih0FcLF5vzCrDhVbTlwb1zvlDXmFj63st8hmm6DtuoS+b6IBFjJuLJ+gbrIxncpqMG6wecQYopXn3dYQSjWezyvffESHbg55XlfmF1PG4RNAvk9tHB94H1lISX4bF1TLMe+dItIbCzrBPKFqBV"
 pwd = "Cp2eWJAb"
 r = RNCryptor.RNCryptor()
 
